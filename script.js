ec5bdb053fdc5ed4e9253ace1025b97f124ec964
R Rohan Shalom <rohanshalom05@gmail.com>
2025-03-01 21:09:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const messageContainer = document.getElementById('message-container');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
-// Prompt user for their name
+
 const name = prompt('What is your name?') || 'Anonymous';
 appendMessage('You joined', 'system');
 socket.emit('new-user', name);
